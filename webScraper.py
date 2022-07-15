5f65693891e488ee3a8061d7d288d50d8c609795
Gabrielle Lake <gabrielle.k.lake@gmail.com>
2022-07-15 14:34:43 -0700

    Revert "Updating TODO list with next steps for retraining ML models"
    This reverts commit a3a8a566141bec5953b1a13a43b132808969d53c.

--- webScraper.py
+++ webScraper.py
@@ -52,15 +52,6 @@ sentence = "usually yes, also there is waf bypass tools on hackbar. In general, 
 testInput = tfIdf.transform([sentence])
 print(testInput)
 
-# TODO: create two separate directories, one for positive HTML files and one for negative HTML files
-# TODO: Use a for loop to loop through the HTML files and extract & clean text from them
-# TODO: append the text to the X list and append a 1/0 to the Y list
-# TODO: tfIdf = Transformer()
-# TODO: tfIdf.fit(X)
-# TODO: dump tfIdf to a pickle file
-# TODO: xTrain, xTest, yTrain, yTest = sklearn.split(x, y)
-# TODO: tfIdg.transform(xTrain)
-# TODO: tfIdg.transform(xTest)
-# TODO: train classifier(s)
-# TODO: test classifier(s)
-# TODO: dump best working classifier to pickle file
+# TODO: Append a y- or n- to the front or end of each .htm file name to distinguish if its related to criminal hacking
+# TODO: Use the y-/n- to create the label vector(y)
+# TODO: Use method from this link: https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
