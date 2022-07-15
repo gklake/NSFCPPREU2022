0f0357d8c52226b32d4c8610cfb10e690b32a569
Gabrielle Lake <gabrielle.k.lake@gmail.com>
2022-07-15 14:36:58 -0700

    Updating TODO list with next steps for retraining ML models

--- classifier.py
+++ classifier.py
@@ -19,3 +19,16 @@ lemmatizer = WordNetLemmatizer()
 
 with open('tfIdf_vectorizer.pickle', 'rb') as file:
 	tfIdfVectorizedData = pickle.load(file)
+
+# TODO: create two separate directories, one for positive HTML files and one for negative HTML files
+# TODO: Use a for loop to loop through the HTML files and extract & clean text from them
+# TODO: append the text to the X list and append a 1/0 to the Y list
+# TODO: tfIdf = Transformer()
+# TODO: tfIdf.fit(X)
+# TODO: dump tfIdf to a pickle file
+# TODO: xTrain, xTest, yTrain, yTest = sklearn.split(x, y)
+# TODO: tfIdg.transform(xTrain)
+# TODO: tfIdg.transform(xTest)
+# TODO: train classifier(s)
+# TODO: test classifier(s)
+# TODO: dump best working classifier to pickle file
