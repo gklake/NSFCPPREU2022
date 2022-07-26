a99397e0fe5b13014e6512ca0f7098125c0cd138
Gabrielle Lake <gabrielle.k.lake@gmail.com>
2022-07-26 16:15:24 -0700

    updating RegEx

--- outgoingLinkFinder.py
+++ outgoingLinkFinder.py
@@ -1,5 +1,4 @@
 import re
-import html
 import urllib.request
 import bs4 as bs
 
@@ -7,13 +6,13 @@ url = "https://stackoverflow.com/questions/70807518/does-not-match-size-of-targe
 request = urllib.request.urlopen(url)
 splitRootAndPath = url.split(".com")
 rawHTML = request.read()
-rootURL = splitRootAndPath[0]
+rootURL = splitRootAndPath[0].replace("http://", "").replace("https://", "")
+print(url)
 print(rootURL)
 
 articleHTML = bs.BeautifulSoup(rawHTML, 'html.parser')
-# allLinks = articleHTML.find_all('a')
-# allLinks = re.findall(r'[a-zA-Z0-9.:/-]+[.]com[a-zA-Z0-9:/-]*', rawHTML.decode('utf-8'))
-allLinks = re.findall(r'"((http|ftp)s?://.*?)"', rawHTML.decode('utf-8'))
+# allLinks = articleHTML.find_all('a') # finds all <a> tags
+allLinks = re.findall(r'(http:\/\/|https:\/\/)?([a-zA-Z0-9.-]+)([.])(onion|com)(\/[a-zA-Z0-9\/-]*)*', rawHTML.decode('utf-8'))
 
 '''
 # All Regex I've Tried:
@@ -25,7 +24,10 @@ allLinks = re.findall(r'"((http|ftp)s?://.*?)"', rawHTML.decode('utf-8'))
    # "((http|ftp)s?://.*?)"
       # Returns the link with everything, but in a tuple with 'http': ('https://cdn.sstatic.net/Sites/stackoverflow/Img/favicon.ico?v=ec617d715196', 'http')
       # The filtering doesn't work, len(allLinks) = len(outgoingLinks). Presumably due to the link being returned in a tuple
-
+   # ((http|ftp)s?:\/\/.*?[.](onion|com))
+   # [a-zA-Z0-9:\/.-]+[.](onion|com)(\/[a-zA-Z0-9\/-]*)*
+   # ([a-zA-Z0-9:\/.-]+)([.])(onion|com)(\/[a-zA-Z0-9\/-]*)*
+   # (http:\/\/|https:\/\/)?([a-zA-Z0-9.-]+)([.])(onion|com)(\/[a-zA-Z0-9\/-]*)*
 '''
 
 print("\n\nAll Links from " + url + ": ")
